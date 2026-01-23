92ff1c88d10a6f34b8b7e59800d2427d1def6bc0
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-01-23 10:17:45 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -3,79 +3,67 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] dist = new int[101];
-	static int ladderCnt;
-	static int snakeCnt;
-	static Map<Integer, Integer> ladderMap;
-	static Map<Integer, Integer> snakeMap;
+	private static final int BOARD_SIZE = 100;
+	private static final int[] board = new int[BOARD_SIZE + 1];
 
 	public static void main(String[] args) throws Exception {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			ladderCnt = Integer.parseInt(st.nextToken());
-			snakeCnt = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 
-			ladderMap = new HashMap<>();
-			snakeMap = new HashMap<>();
-			for (int i = 0; i < ladderCnt; i++) {
+			for (int i = 0; i < N + M; i++) {
 				st = new StringTokenizer(br.readLine());
-				ladderMap.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-			}
-			for (int i = 0; i < snakeCnt; i++) {
-				st = new StringTokenizer(br.readLine());
-				snakeMap.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+				int from = Integer.parseInt(st.nextToken());
+				int to = Integer.parseInt(st.nextToken());
+				board[from] = to;
 			}
 
-			int result = shotDist();
-			bw.write(result + "");
+			bw.write(bfs() + "");
 
 			bw.flush();
 		}
 	}
 
-	private static int shotDist() {
+	private static int bfs() {
+		Queue<Integer> queue = new LinkedList<>();
+		int dist[] = new int[BOARD_SIZE + 1];
 		Arrays.fill(dist, -1);
 
-		Queue<Integer> queue = new LinkedList<>();
 		queue.offer(1);
 		dist[1] = 0;
 
 		while (!queue.isEmpty()) {
 			int curr = queue.poll();
 
-			if (curr == 100) {
+			if (curr == BOARD_SIZE) {
 				return dist[curr];
 			}
 
 			for (int dice = 1; dice <= 6; dice++) {
 				int next = curr + dice;
-				if (next > 100) {
+
+				if (next > BOARD_SIZE) {
 					continue;
 				}
 
-				int finalPos = next;
-				if (ladderMap.containsKey(next)) {
-					finalPos = ladderMap.get(next);
-				} else if (snakeMap.containsKey(next)) {
-					finalPos = snakeMap.get(next);
+				if (board[next] != 0) {
+					next = board[next];
 				}
-				
-				if (dist[finalPos] != -1)
-					continue;
 
-				dist[finalPos] = dist[curr] + 1;
-				queue.offer(finalPos);
+				if (dist[next] == -1) {
+					dist[next] = dist[curr] + 1;
+					queue.offer(next);
+				}
 			}
 		}
-		return dist[100];
+		return -1;
 	}
 }
