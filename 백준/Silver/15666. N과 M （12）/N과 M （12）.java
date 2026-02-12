8488853a5a5e822bddb93216509207c862445987
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-02-12 13:17:11 +0900

    [Silver II] Title: N과 M (12), Time: 324 ms, Memory: 17984 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static int[] arr;
+	static int[] result;
+	static boolean[] visited;
+
+	public static void main(String[] args) throws Exception {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+
+			arr = new int[N];
+			result = new int[M];
+			visited = new boolean[N];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+
+			Arrays.sort(arr);
+			arr = Arrays.stream(arr).distinct().toArray();
+
+			permutation(0, 0);
+
+			bw.flush();
+		}
+	}
+
+	private static void permutation(int start, int depth) {
+		if (depth == M) {
+			for (int num : result) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = start; i < arr.length; i++) {
+			result[depth] = arr[i];
+			permutation(i, depth + 1);
+		}
+	}
+}
