342e6a35748462d6c30353ee59e5d63224455e20
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-03-31 13:56:34 +0900

    [Silver I] Title: 정수 삼각형, Time: 252 ms, Memory: 28068 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -4,9 +4,6 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
-	static int[][] triangle;
-	static int[][] dp;
-
 
 	public static void main(String[] args) throws Exception {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,35 +11,29 @@ public class Main {
 
 			int n = Integer.parseInt(br.readLine());
 
-			triangle = new int[n][n];
-			for (int i = 0; i < n; i++) {
-				String[] split = br.readLine().split(" ");
-				for (int j = 0; j <= i; j++) {
-					triangle[i][j] = Integer.parseInt(split[j]);
-				}
-			}
-
-			dp = new int[n][n];
-			dp[0][0] = triangle[0][0];
+			int[][] dp = new int[n][n];
+			dp[0][0] = Integer.parseInt(br.readLine());
 
 			for (int i = 1; i < n; i++) {
+				String[] split = br.readLine().split(" ");
 				for (int j = 0; j <= i; j++) {
 					if (j == 0) {
-						dp[i][j] = triangle[i][j] + dp[i - 1][j];
+						dp[i][j] = Integer.parseInt(split[j]) + dp[i - 1][j];
 					} else if (j == i) {
-						dp[i][j] = triangle[i][j] + dp[i - 1][j - 1];
+						dp[i][j] = Integer.parseInt(split[j]) + dp[i - 1][j - 1];
 					} else {
-						dp[i][j] = triangle[i][j] + Math.max(dp[i - 1][j - 1], dp[i - 1][j]);
+						dp[i][j] = Integer.parseInt(split[j]) + Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
 					}
 				}
 			}
 
-			int result = Integer.MIN_VALUE;;
+			int max = Integer.MIN_VALUE;
 			for (int i = 0; i < n; i++) {
-				result = Math.max(result, dp[n - 1][i]);
+				if (dp[n - 1][i] > max) {
+					max = dp[n - 1][i];
+				}
 			}
-
-			bw.write(result + "");
+			bw.write(max + "");
 
 			bw.flush();
 		}
