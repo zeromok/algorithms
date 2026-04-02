3c0732be193afc1a1721058c83b52620dc1f1fb2
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-04-02 16:05:12 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 784 ms, Memory: 121016 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,41 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class Main {
+	static int[][] dp;
 
-	public static void main( String[] args ) {
-		Scanner sc = new Scanner( System.in );
+	public static void main(String[] args) throws Exception {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
-		int arrCnt = Integer.parseInt(sc.next()); // N x N 개의 크기 입력 받기
-		int queryCnt = Integer.parseInt(sc.next()); // 질의 개수 입력 받기
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			dp = new int[N + 1][N + 1];
 
-		// 원본 배열 선언
-		int[][] originArr = new int[ arrCnt + 1 ][ arrCnt + 1 ];
-		// 원본 배열 초기화
-		for ( int i = 1; i <= arrCnt; i++ ) {
-			for ( int j = 1; j <= arrCnt; j++ ) {
-				originArr[ i ][ j ] = Integer.parseInt(sc.next());
+			for (int i = 1; i <= N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 1; j <= N; j++) {
+					int value = Integer.parseInt(st.nextToken());
+					dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + value;
+				}
 			}
-		}
 
-		// 합 배열 선언
-		int[][] sumArr = new int[arrCnt + 1][arrCnt + 1];
-		// 합 배열 초기화
-		for ( int i = 1; i <= arrCnt; i++ ) {
-			for ( int j = 1; j <= arrCnt; j++ ) {
-				sumArr[ i ][ j ] =
-					sumArr[ i ][ j - 1 ] + sumArr[ i - 1 ][ j ] - sumArr[ i - 1 ][ j - 1 ] + originArr[ i ][ j ];
+			while (M-- > 0) {
+				st = new StringTokenizer(br.readLine());
+				int x1 = Integer.parseInt(st.nextToken());
+				int y1 = Integer.parseInt(st.nextToken());
+				int x2 = Integer.parseInt(st.nextToken());
+				int y2 = Integer.parseInt(st.nextToken());
+
+				int result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+				bw.write(result + "\n");
 			}
+
+			bw.flush();
 		}
-
-		// 질의 받은 후 결과 출력
-		for ( int i = 0; i < queryCnt; i++ ) {
-			int x1 = Integer.parseInt(sc.next());
-			int y1 = Integer.parseInt(sc.next());
-			int x2 = Integer.parseInt(sc.next());
-			int y2 = Integer.parseInt(sc.next());
-
-			System.out.println(sumArr[ x2 ][ y2 ] - sumArr[ x1 - 1 ][ y2 ] - sumArr[ x2 ][ y1 - 1 ] + sumArr[ x1 - 1 ][ y1 - 1 ]);
-		}
-
 	}
-
-} // end
+}
