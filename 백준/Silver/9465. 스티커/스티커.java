3ef3d9af57c59b610d9b400819870bd683af05d9
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-04-04 11:17:59 +0900

    [Silver I] Title: 스티커, Time: 684 ms, Memory: 106648 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -5,36 +5,31 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws Exception {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
 			int T = Integer.parseInt(br.readLine());
-
 			while (T-- > 0) {
 				int n = Integer.parseInt(br.readLine());
-				int[][] stickers = new int[2][n];
 				int[][] dp = new int[2][n];
 
 				for (int i = 0; i < 2; i++) {
 					StringTokenizer st = new StringTokenizer(br.readLine());
 					for (int j = 0; j < n; j++) {
-						stickers[i][j] = Integer.parseInt(st.nextToken());
+						dp[i][j] = Integer.parseInt(st.nextToken());
 					}
 				}
 
-				dp[0][0] = stickers[0][0];
-				dp[1][0] = stickers[1][0];
-
 				if (n > 1) {
-					dp[0][1] = stickers[0][1] + dp[1][0];
-					dp[1][1] = stickers[1][1] + dp[0][0];
+					dp[0][1] += dp[1][0];
+					dp[1][1] += dp[0][0];
 				}
 
 				for (int i = 2; i < n; i++) {
-					dp[0][i] = stickers[0][i] + Math.max(dp[1][i - 1], dp[1][i - 2]);
-					dp[1][i] = stickers[1][i] + Math.max(dp[0][i - 1], dp[0][i - 2]);
+					dp[0][i] += Math.max(dp[1][i - 1], dp[1][i - 2]);
+					dp[1][i] += Math.max(dp[0][i - 1], dp[0][i - 2]);
 				}
 
 				bw.write(Math.max(dp[0][n - 1], dp[1][n - 1]) + "\n");
