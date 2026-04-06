272f800f03524e7d49b9e93d56fd4a5cbddb791d
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-04-06 11:57:49 +0900

    [Silver I] Title: 정수 삼각형, Time: 244 ms, Memory: 26116 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int[][] dp;
@@ -11,16 +12,15 @@ public class Main {
 			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
 			int n = Integer.parseInt(br.readLine());
-
 			dp = new int[n][n];
 			dp[0][0] = Integer.parseInt(br.readLine());
 
 			for (int i = 1; i < n; i++) {
-				String[] split = br.readLine().split(" ");
-				for (int j = 0; j < split.length; j++) {
-					int value = Integer.parseInt(split[j]);
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j <= i; j++) {
+					int value = Integer.parseInt(st.nextToken());
 					if (j == 0) {
-						dp[i][j] = value + dp[i - 1][j];
+						dp[i][j] = value + dp[i - 1][0];
 					} else if (i == j) {
 						dp[i][j] = value + dp[i - 1][j - 1];
 					} else {
@@ -35,7 +35,6 @@ public class Main {
 					max = dp[n - 1][i];
 				}
 			}
-
 			bw.write(max + "");
 
 			bw.flush();
