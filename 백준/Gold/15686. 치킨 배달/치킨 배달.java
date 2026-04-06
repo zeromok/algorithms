6ac5fde127bccb769bbdf66bc3e6703a024dc979
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-04-06 15:51:12 +0900

    [Gold V] Title: 치킨 배달, Time: 256 ms, Memory: 25380 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Point {
+		int r, c;
+
+		public Point(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
+
+	static int N;
+	static int M;
+	static int minCityDist = Integer.MAX_VALUE;
+
+	static List<Point> houses = new ArrayList<>();
+	static List<Point> chickens = new ArrayList<>();
+	static List<Point> selected = new ArrayList<>();
+
+	public static void main(String[] args) throws Exception {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+
+			for (int i = 1; i <= N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 1; j <= N; j++) {
+					int value = Integer.parseInt(st.nextToken());
+					if (value == 1) {
+						houses.add(new Point(i, j));
+					} else if (value == 2) {
+						chickens.add(new Point(i, j));
+					}
+				}
+			}
+
+			dfs(0, 0);
+
+			bw.write(minCityDist + "");
+
+			bw.flush();
+		}
+	}
+
+	private static void dfs(int idx, int count) {
+		if (count == M) {
+			int totalDist = 0;
+
+			for (Point house : houses) {
+				int minDist = Integer.MAX_VALUE;
+
+				for (Point chicken : selected) {
+					int dist = Math.abs(house.r - chicken.r) + Math.abs(house.c - chicken.c);
+					minDist = Math.min(minDist, dist);
+				}
+
+				totalDist += minDist;
+			}
+
+			minCityDist = Math.min(minCityDist, totalDist);
+			return;
+		}
+
+		for (int i = idx; i < chickens.size(); i++) {
+			selected.add(chickens.get(i));
+			dfs(i + 1, count + 1);
+			selected.remove(selected.size() - 1);
+		}
+	}
+}
