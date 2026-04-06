ee87db178baeaef2da64afbb31990a8add86dd2a
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-04-06 13:48:41 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 796 ms, Memory: 120840 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -15,6 +15,7 @@ public class Main {
 			int M = Integer.parseInt(st.nextToken());
 
 			int[][] dp = new int[N + 1][N + 1];
+
 			for (int i = 1; i <= N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 1; j <= N; j++) {
@@ -30,7 +31,8 @@ public class Main {
 				int x2 = Integer.parseInt(st.nextToken());
 				int y2 = Integer.parseInt(st.nextToken());
 
-				bw.write(dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] + "\n");
+				int result = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1];
+				bw.write(result + "\n");
 			}
 
 			bw.flush();
