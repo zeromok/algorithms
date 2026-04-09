5ddd2408fd27aa0e4ee67b2ef7f765b443293a50
Byeong-mok Park <99172504+zeromok@users.noreply.github.com>
2026-04-09 11:35:41 +0900

    [Silver I] Title: 스티커, Time: 680 ms, Memory: 107372 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -5,12 +5,13 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws Exception {
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
 
 			int T = Integer.parseInt(br.readLine());
+
 			while (T-- > 0) {
 				int n = Integer.parseInt(br.readLine());
 				int[][] dp = new int[2][n];
